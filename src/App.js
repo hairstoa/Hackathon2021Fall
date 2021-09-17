3dcc97b000ed0c10dec8541f3e33006b6995f83a
ryannicoletti <r.nicoletti1@gmail.com>
2021-09-17 13:19:47 -0400

    added user location tracking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import './App.css';
-import Map from './Map/Map.js';
+import "./App.css";
+import Map from "./Map/Map.js";
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+import mapboxgl from "mapbox-gl";
 
-function App() {
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiamFpbWVqdXN0byIsImEiOiJja3RubHdxa2IwNDBkMm9vMzBqMmsxZzV1In0.GrfCgBDgjmUf1ouM7xBOSQ";
+const App = () => {
+  const setUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setViewport({
+        ...viewport,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 10,
+      });
+    });
+  };
+
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 42.35,
+    longitude: -70.9,
+    zoom: 9,
+  });
+
   return (
     <div className="App">
-      <Map /> 
+      <button onClick={() => setUserLocation()}>My Location</button>
+      <ReactMapGL
+        mapboxApiAccessToken={mapboxgl.accessToken}
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      />
     </div>
   );
-}
+};
 
 export default App;
+
+// import "./App.css";
+// import Map from "./Map/Map.js";
+// import React from "react";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Map />
+//     </div>
+//   );
+// }
+
+// export default App;
