5eaad4fced958f10ee31f12ac156f0c9f257686b
jaimejusto <jaimejusto@gmail.com>
2021-09-17 21:30:48 -0700

    added county line layer to map

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,6 +2,8 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./Map.css";
+import countylines from "../data/CountyLayer.geojson";
+
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamFpbWVqdXN0byIsImEiOiJja3RubHdxa2IwNDBkMm9vMzBqMmsxZzV1In0.GrfCgBDgjmUf1ouM7xBOSQ";
@@ -53,6 +55,31 @@ function Map() {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return;
+
+    // add county polygons
+    map.current.on("load", () => {
+        
+      // add source for county polygons
+      map.current.addSource("counties", {
+            "type": "geojson",
+            "data": countylines
+      });
+
+      // add layer showing county polygons
+      map.current.addLayer({
+          "id": "county-layer",
+          "type": "fill",
+          "source": "counties",
+          "paint": {
+              "fill-color": "rgba(240, 52, 52, 0.1)",
+              "fill-outline-color": "rgba(240, 52, 52, 1)"
+          }
+      });
     });
   });
 
